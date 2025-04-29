c6895f28cfa679dec9e11e4ac3f4511311d7d2c2
makhsumovM <muhammadmakhsumov1@gmail.com>
2025-04-30 00:30:37 +0500

    Refactor token utilities and enhance ProtectedRoute logic
    Added token decoding and expiration check functions to the utility. Updated ProtectedRoute to validate tokens with the new utility methods, improving security and reliability.

--- src/Utils/token.js
+++ src/Utils/token.js
@@ -1,13 +1,34 @@
+import {jwtDecode} from "jwt-decode";
+
 function getToken() {
-  return localStorage.getItem("access_token");
+    return localStorage.getItem("access_token");
 }
 
 function saveToken(token) {
-  localStorage.setItem("access_token", token);
+    localStorage.setItem("access_token", token);
 }
 
 function destroyToken() {
-  localStorage.removeItem("access_token");
+    localStorage.removeItem("access_token");
 }
 
-export { getToken, saveToken, destroyToken };
+function decodedToken() {
+    try {
+        return jwtDecode(getToken())
+    } catch (e) {
+        return null;
+    }
+}
+
+function isTokenExpired(token) {
+    try {
+        const decoded = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+        return decoded.exp < currentTime;
+    } catch (e) {
+        return true;
+    }
+
+}
+
+export {getToken, saveToken, destroyToken, isTokenExpired, decodedToken};
